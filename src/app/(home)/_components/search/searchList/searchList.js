798f62c817866c46bbfc8d9db9c08694a0a4268e
Michael <miselrkba@gmail.com>
2024-07-23 07:27:35 +0200

    fix: id zistenia 16, 18, 19, 20, 47, 48

--- src/app/(home)/_components/search/searchList/searchList.js
+++ src/app/(home)/_components/search/searchList/searchList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from '../card/card';
 
+
 const SearchList = ({ filteredPages }) => {
   const filtered = filteredPages.map(page => <Card key={page.name} page={page} />);
   return (
-    <div>
+    <ul role="list" aria-live="polite" className="search-results-list">
       {filtered}
-    </div>
+    </ul>
   );
 }
 
