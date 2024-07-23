bc477c7e13957e7970179ea3d869b4aabecd449d
Michael <miselrkba@gmail.com>
2024-07-23 07:31:23 +0200

    fix: icon path

--- src/app/(home)/_components/arrow-up-btn/arrow.js
+++ src/app/(home)/_components/arrow-up-btn/arrow.js
@@ -1,6 +1,6 @@
 import { scrollToTop } from "@/utils/scroll";
 import { TextButton } from "@eslovensko/idsk-react";
-import SvgArrowUp from '/node_modules/@eslovensko/idsk-core/dist/assets/svgIcons/Navigation/arrow_upward.svg';
+import SvgArrowUp from '/node_modules/@eslovensko/idsk-core/src/assets/svgIcons/Navigation/arrow_upward.svg';
 
 const ArrowUpBtn = () => {
     return (
