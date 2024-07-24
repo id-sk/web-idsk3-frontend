af9a543b8d2ff1dfdb974018d8f4145666acaee8
marosbar <maros.baranek@csirt.sk>
2024-07-24 15:55:53 +0200

    fix 51

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
-import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import '../styles/index.css'
 
-
-const sourceSansPro = Source_Sans_3({ subsets: ["latin"] });
 
 export const metadata = {
   title: "IDSK v3",
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="sk">
-      <body className={sourceSansPro.className}>
+      <body>
         {children}  
       </body>
     </html>
