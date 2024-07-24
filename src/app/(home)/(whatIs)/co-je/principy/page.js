c77e897234123cca7eb7f95c6667eac1c9b8406d
Miselrkba <68853293+Miselrkba@users.noreply.github.com>
2024-07-24 12:44:23 +0200

    fix: 25, 26, 27, 28, 61 (#16)

--- src/app/(home)/(whatIs)/co-je/principy/page.js
+++ src/app/(home)/(whatIs)/co-je/principy/page.js
@@ -7,9 +7,9 @@ const PrincipyPage = () => {
     return (
         <div className="flex flex-col self-stretch my-8 pb-2.5 leading-7">
             <title>Princípy</title>
-            <div className="w-full text-5xl font-black text-black leading-[55.2px] max-md:max-w-full max-md:text-4xl">
+            <h1>
                 10 princípov vytvárania služieb podľa ID-SK
-            </div>
+            </h1>
             <div className="mt-8 w-full text-xl tracking-wide text-black max-md:max-w-full">
                 <ol>
                     <li>
@@ -95,36 +95,26 @@ const PrincipyPage = () => {
                     </li>
                 </ol>
                 <br />
-                
-                <span id="uzitocnost" className="text-4xl font-black leading-10">
+
+                <h3 id="uzitocnost" className="mb-4">
                     1. Začnite potrebami používateľov (princíp užitočnosti)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Dizajn služby začína identifikáciou potrieb používateľov. Ak neviete,
                 aké sú potreby používateľov, nepodarí sa vám vytvoriť správnu službu.
                 Nevychádzajte z domnienok. Používateľský prieskum, analýza dát a
                 informácie priamo od používateľov vám pomôžu vybudovať zázemie,
                 na ktorom môžete postaviť službu, ktorá bude spĺňať svoj účel a bude
                 užitočná.
-                <br />
-                <br />
-                <span id="stabilita" className="text-4xl font-black leading-10">
+                <h3 id="stabilita" className="my-4">
                     2. Opätovne používajte a zdieľajte (princíp stability a otvorenosti)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Platformy a registre je dôležité vytvárať tak, aby tvorili funkčný
                 nemenný základ, ktorý je opätovne použiteľný. Poskytovanie a vzájomné
                 zdieľanie zdrojov môže výrazne uľahčiť prácu ďalším tvorcom
                 elektronických služieb.
-                <br />
-                <br />
-                <span id="realnost" className="text-4xl font-black leading-10">
+                <h3 id="realnost" className="my-4">
                     3. Inšpirujte sa realitou (princíp reálnosti)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Pri vytváraní elektronických služieb môžete vychádzať zo spôsobov
                 používania existujúcich služieb v reálnom svete. Aby bola služba
                 kvalitná, pokračujte v jej dizajnovaní aj po jej dokončení, a to
@@ -134,36 +124,24 @@ const PrincipyPage = () => {
                 reálne používanie. Základným nástrojom každej elektronickej služby je
                 dátová analytika, ktorá by mala byť jednoducho čitateľná a neustále
                 zapnutá.
-                <br />
-                <br />
-                <span id="jednoduchost" className="text-4xl font-black leading-10">
+                <h3 id="jednoduchost" className="my-4">
                     4. Náročná tvorba vedie k jednoduchým službám (princíp jednoduchosti)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Elektronizácia je príležitosťou na zjednodušenie služieb používateľom.
                 Pri dizajne služieb vnímajte širší kontext, nielen existujúci postup.
                 Investujte čas do opätovného prepracovania procesov a neuspokojte sa
                 s názorom, ktorý zachováva existujúci stav.
-                <br />
-                <br />
-                <span id="agilita" className="text-4xl font-black leading-10">
+                <h3 id="agilita" className="my-4">
                     5. Začnite v malom (princíp agility)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Sprístupnite používateľom svoju službu čo najskôr a získajte spätnú
                 väzbu od koncových používateľov. Je lepšie rozvíjať službu postupne a
                 po malých častiach, ako spúšťať veľké celky. Znížite tým riziká a
                 získate cenné informácie, ktoré môžu ovplyvniť vaše ďalšie investície
                 do rozvoja.
-                <br />
-                <br />
-                <span id="pristupnost" className="text-4xl font-black leading-10">
+                <h3 id="pristupnost" className="my-4">
                     6. ID-SK je pre každého (princíp prístupnosti)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Prístupný dizajn je dobrý dizajn. Elektronické služby vytvorené podľa
                 ID-SK by mali byť inkluzívne, zrozumiteľné a jednoducho čitateľné. Je
                 potrebné si pripomínať, že elektronické služby sa vytvárajú s konkrétnym
@@ -172,37 +150,25 @@ const PrincipyPage = () => {
                 informačné technológie každý deň. Ľudia ktorí najviac potrebujú naše
                 služby sú často ľudia, ktorým sa zdá ich používanie najťažšie. Práve
                 tieto osoby by mali tvoriť časť testovacej skupiny.
-                <br />
-                <br />
-                <span id="kontext" className="text-4xl font-black leading-10">
+                <h3 id="kontext" className="my-4">
                     7. Rešpektovanie kontextu okolnosti použitia (princíp kontextu)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Pri tvorbe elektronických služieb je potrebné zohľadňovať okolnosti a
                 kontext, v ktorom ľudia služby budú používať. Dizajn musí adresovať
                 rozdielne potreby podľa času, miesta a zariadenia, v rámci ktorých sa
                 bude služba primárne vykonávať.
-                <br />
-                <br />
-                <span id="holistika" className="text-4xl font-black leading-10">
+                <h3 id="holistika" className="my-4">
                     8. Vytvárajte elektronické služby, nie webové stránky (princíp
                     holistického pohľadu)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Pri tvorbe služby vnímajte ich multikanálový kontext. Netvoríte len
                 webovú stránku, ale navrhujete zážitok používateľa naprieč rôznymi
                 digitálnymi aj nedigitálnymi kanálmi. Zohľadnite zistenia
                 zo zákazníckeho prieskumu aj v iných kanáloch, aby ste vytvorili
                 kvalitnejšiu službu.
-                <br />
-                <br />
-                <span id="konzistentnost" className="text-4xl font-black leading-10">
+                <h3 id="konzistentnost" className="my-4">
                     9. Buďte konzistentní (princíp konzistentnosti)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Princíp konzistentnosti spočíva v používaní rovnakého jazyka, výrazov
                 uvedených v slovníku a rovnakých dizajnových vzorov. Uplatňovaním tohto
                 princípu sa pri tvorbe služieb zabezpečí jednak vizuálna jednotnosť a
@@ -211,13 +177,9 @@ const PrincipyPage = () => {
                 používateľ nadobudne zručnosť v ich používaní. V takom prípade ich bude
                 používať opakovane a tieto služby budú pre neho skutočne efektívnym
                 spôsobom využívania potrebných služieb.
-                <br />
-                <br />
-                <span id="otvorenost" className="text-4xl font-black leading-10">
+                <h3 id="otvorenost" className="my-4">
                     10. Tvorte otvorene, zvýšite tým kvalitu (princíp otvorenosti)
-                </span>
-                <br />
-                <br />
+                </h3>
                 Zdieľajte vaše návrhy, nápady, zámery, neúspechy a zdrojové kódy
                 s komunitou. Otvorenosť podporuje kreativitu a zlepšuje kvalitu výstupov
                 cez skorú spätnú väzbu. Keďže tvorba elektronických služieb je spravidla
