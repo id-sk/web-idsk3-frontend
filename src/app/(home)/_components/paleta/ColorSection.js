82143906ba5366229febc1522b8bb0d0bf9fe0da
marosbar <maros.baranek@csirt.sk>
2024-08-01 12:39:28 +0200

    changed font-size for <p> regard rules

--- src/app/(home)/_components/paleta/ColorSection.js
+++ src/app/(home)/_components/paleta/ColorSection.js
@@ -7,7 +7,7 @@ const ColorSection = ({ title, description, children }) => {
   return (
     <section className="mt-20 max-md:mt-10">
       <h2 className="w-full text-4xl font-black text-black max-md:max-w-full">{title}</h2>
-      <p className="mt-5 w-full text-xl tracking-wide leading-7 text-black max-md:max-w-full">{description}</p>
+      <p className="mt-5 w-full text-custom-19 tracking-wide leading-7 text-black max-md:max-w-full">{description}</p>
       {children}
     </section>
   );
