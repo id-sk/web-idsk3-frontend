170c6f084f22e30da652fef88ea5c97b8f6eb4d6
marosbar <maros.baranek@csirt.sk>
2024-08-06 11:50:46 +0200

    splitted pages and created

--- src/app/(home)/(idsk)/komponenty/razcestnik-textovy/page.js
+++ src/app/(home)/(idsk)/komponenty/razcestnik-textovy/page.js
@@ -11,7 +11,6 @@ const NavigationBarPage = () => {
       <title>Rázcestník</title>
       <div className="flex gap-5 max-md:flex-col max-md:gap-0">
         <section className="flex flex-col ml-5 w-[60%] max-md:ml-0 max-md:w-full">
-          {/* *Textovy* */}
           <article className="flex flex-col grow pt-16 pb-8 max-md:mt-5 max-md:max-w-full">
             <h1 className="text-5xl font-black text-black leading-[55.2px] max-md:max-w-full max-md:text-4xl">Textový rázcestník</h1>
             <p className="mt-5 text-2xl tracking-normal leading-9 text-black max-md:max-w-full">
@@ -67,78 +66,6 @@ const NavigationBarPage = () => {
               height={100}
               quality={100}
               alt="text-razcestnik"
-              className="w-full h-full"
-            />
-          </div>
-          {/* *Vertikalny* */}
-          <article className="flex flex-col grow pt-12 pb-8 max-md:mt-5 max-md:max-w-full">
-            <h1 className="text-5xl font-black text-black leading-[55.2px] max-md:max-w-full max-md:text-4xl">Vertikálny rázcestník</h1>
-            <p className="mt-5 text-2xl tracking-normal leading-9 text-black max-md:max-w-full">
-              Vertikálny rázcestník má formu jednoduchej dlaždice usporiadanej vertikálne, ktorá je zložená z nadpisu, popisu poprípade ikony.
-              Jeho účelom je prehľadne usporiadať pre používateľa odkazy na súvisiaci obsah, ktorý je rozmiestnený na rôznych, samostatných podstránkach.
-              Rázcestník je jedným z komponentov, ktoré navigujú používateľa k obsahu vašej stránky.
-            </p>
-            <h2 className="mt-20 text-4xl font-black text-black max-md:mt-10 max-md:max-w-full">Použitie vertikálneho rázcestníka </h2>
-            <p className="mt-5 text-xl tracking-wide leading-7 text-black max-md:max-w-full">
-              Rázcestník je jedným z komponentov, ktoré navigujú používateľa k obsahu vašej stránky.
-              Použitím rázcestníka spolu s nadpisom a podnadpisom dáte používateľovi najavo,
-              ktorý obsah (a ako) spolu na stránke súvisí.
-              Pri definovaní nadpisu a podnadpisu buďte struční a vecní.
-              Rázcestník môžete štrukturovať do jedného alebo viacerých stĺpcov, musíte však dodržať pravidlá pre rozloženie stránky (grid).
-            </p>
-            <h2 className="mt-8 mb-4">Varianty</h2>
-            <p className="idsk-subtitle mb-4">
-              Komponent umožňuje použitie 4 variantov:
-            </p>
-            <ol className='idsk-subtitle mb-4'>
-              <li>1. S akciou</li>
-              <li>2. Vybraný stav</li>
-              <li>3. S ikonou</li>
-              <li>4. S obrázkom</li>
-            </ol>
-          </article>
-          <div className="my-2 w-[100%] h-[100%] ">
-            <Image
-              src="/images/razcestnik/vertikalny-razcestnik.svg"
-              width={100}
-              height={100}
-              quality={100}
-              alt="vertikalny-razcestnik"
-              className="w-full h-full"
-            />
-          </div>
-          {/* *Horizontalny* */}
-          <article className="flex flex-col grow pt-16 pb-8 max-md:mt-5 max-md:max-w-full">
-            <h1 className="text-5xl font-black text-black leading-[55.2px] max-md:max-w-full max-md:text-4xl">Horizontálny rázcestník</h1>
-            <p className="mt-5 text-2xl tracking-normal leading-9 text-black max-md:max-w-full">
-              Horizontálny rázcestník má formu jednoduchej dlaždice usporiadanej horizontálne, ktorá je zložená z nadpisu, popisu a oddeľovacej čiary.
-              Jeho účelom je prehľadne usporiadať pre používateľa odkazy na súvisiaci obsah, ktorý je rozmiestnený na rôznych, samostatných podstránkach.
-              Rázcestník je jedným z komponentov, ktoré navigujú používateľa k obsahu vašej stránky.
-            </p>
-            <h2 className="mt-20 text-4xl font-black text-black max-md:mt-10 max-md:max-w-full">Použitie horizontálenho rázcestníka </h2>
-            <p className="mt-5 text-xl tracking-wide leading-7 text-black max-md:max-w-full">
-              Rázcestník je jedným z komponentov, ktoré navigujú používateľa k obsahu vašej stránky.
-              Použitím rázcestníka spolu s nadpisom a podnadpisom dáte používateľovi najavo, ktorý obsah (a ako) spolu na stránke súvisí.
-              Pri definovaní nadpisu a podnadpisu buďte struční a vecní. Rázcestník môžete štrukturovať do jedného alebo viacerých stĺpcov,
-              musíte však dodržať pravidlá pre rozloženie stránky (grid).
-            </p>
-            <h2 className="mt-8 mb-4">Varianty</h2>
-            <p className="idsk-subtitle mb-4">
-              Komponent umožňuje použitie 4 variantov:
-            </p>
-            <ol className='idsk-subtitle mb-4'>
-              <li>1. S ikonou</li>
-              <li>2. S šípkou</li>
-              <li>3. S ikonou a šípkou</li>
-            </ol>
-          </article>
-          <div className="my-2 w-[100%] h-[100%] ">
-            <Image
-              src="/images/razcestnik/horizontalny-razcestnik.svg"
-              width={100}
-              height={100}
-              quality={100}
-              alt="horizontalny-razcestnik"
               className="w-full h-full"
             />
           </div>
