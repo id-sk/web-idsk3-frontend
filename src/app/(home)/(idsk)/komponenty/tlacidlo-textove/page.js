991eb2c62b151ac0030203ab24f6bdb5169887bd
marosbar <maros.baranek@csirt.sk>
2024-08-06 15:05:10 +0200

    spllited pages

--- src/app/(home)/(idsk)/komponenty/tlacidlo-textove/page.js
+++ src/app/(home)/(idsk)/komponenty/tlacidlo-textove/page.js
@@ -8,26 +8,18 @@ import React from 'react';
 const ButtonPage = () => {
     return (
         <div className="component-page">
-            <title>Tlačidlo primárne</title>
+            <title>Tlačidlo - Textové</title>
             <h1 className="my-8">
-                Tlačidlo - Primárne
+                Tlačidlo - Textové
             </h1>
             <p className="idsk-subtitle mb-16">
-                Primárne tlačidlo predstavuje hlavné akčné prvky, ktoré majú najväčší vplyv na používateľskú interakciu. Tlačidlá sú obvykle umiestnené v dôležitých a strategických oblastiach rozhrania používateľa a majú významný vplyv na používateľskú skúsenosť.
+            Textové tlačidlo slúži na akcie s najnižšou prioritou v rámci používateľského rozhrania. Komponent je použitý zvyčajne spolu s primárnym alebo sekundárnym tlačidlom. Je navrhnutý tak aby bol ľahko viditeľný aj pre ľudí so zrakovým obmedzením.
             </p>
-            <div className="mb-10">
-                <Link href={'https://komponenty.idsk3.gov.sk/components/button/'}>
-                    <ArticleCard>
-                        <h4>Kód komponentu</h4>
-                        <p className="my-1">HTML verzia na prevzatie</p>
-                    </ArticleCard>
-                </Link>
-            </div>
             <h2 className="mb-4">
-                Použitie primárnych tlačidiel
+            Použitie textových tlačidiel 
             </h2>
             <p className="idsk-subtitle mb-8">
-                Používajú sa na vykonanie hlavnej akcie na stránke. Snažte sa vyhnúť použitiu viacerých hlavných tlačidiel na jednej stránke. Viaceré tlačidlá môžu u používateľa vyvolať zmätenosť a problémy s výberom tlačidla k použitiu.
+            Komponent je umiestňovaný na miesta, kde používateľ očakáva akciu s nižšou interakčnou prioritou. Zvyčajne je využívaný spolu s primárnym a sekundárnym tlačidlom. Tlačidlo musí obsahovať zrozumiteľný a výstižný popis, pomocou ktoré používateľ bude vedieť akú akciu ide vykonať.
             </p>
             <h2 className="mt-8 mb-4">
                 Varianty
@@ -39,9 +31,10 @@ const ButtonPage = () => {
                 <li>1. Základný typ</li>
                 <li>2. Úspešný typ</li>
                 <li>3. Upozorňovací typ</li>
+                <li>4. Kontrastný typ</li>
             </ol>
             <Image
-                src="/images/button/primarne/varianty-primarnych-tlacidiel.png"
+                src="/images/button/textove/varianty-textovych-tlacidiel.png"
                 width={1100}
                 height={250}
                 quality={100}
@@ -52,10 +45,10 @@ const ButtonPage = () => {
                 Nastavenie
             </h2>
             <p className="idsk-subtitle mb-4">
-                1. Varianty tlačidla základný/s ikonou vľavo /s ikonou vprav
+                1. Varianty tlačidla základný/s ikonou vľavo/s ikonou vpravo
             </p>
             <Image
-                src="/images/button/primarne/s-ikonou.png"
+                src="/images/button/textove/textove-ikona.png"
                 width={1100}
                 height={250}
                 quality={100}
@@ -63,10 +56,10 @@ const ButtonPage = () => {
                 className="mt-4 mb-8"
             />
             <p className="idsk-subtitle mb-4">
-                2. Varianty tlačidla aktívny stav/vybraný stav/stlačený stav/neaktívny stav
+                2. Varianty tlačidla aktívny stav/stlačený stav/neaktívny stav
             </p>
             <Image
-                src="/images/button/primarne/stav.png"
+                src="/images/button/textove/textove-aktivne.png"
                 width={1100}
                 height={250}
                 quality={100}
