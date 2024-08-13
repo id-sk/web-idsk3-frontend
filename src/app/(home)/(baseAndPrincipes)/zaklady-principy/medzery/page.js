5a40675fe501e8bbbf57bee59fa3817259e613f2
marosbar <maros.baranek@csirt.sk>
2024-08-13 09:35:32 +0200

    added leading

--- src/app/(home)/(baseAndPrincipes)/zaklady-principy/medzery/page.js
+++ src/app/(home)/(baseAndPrincipes)/zaklady-principy/medzery/page.js
@@ -23,7 +23,7 @@ const GapsPage = () => {
         </h1>
         </header>
         <main id="main-content" role="main">
-        <p className="mt-5 text-2xl tracking-normal leading-7 text-black max-md:max-w-full">
+        <p className="mt-5 text-2xl tracking-normal leading-9 text-black max-md:max-w-full">
           Medzery sú dôležité pri určovaní vzťahou medzi jednotlivými prvky a prispieva k celkovej prehľadnosti a zrozumiteľnosti dizajnu. Medzera môže byť použitá na oddelenie jedného prvku od druhého, aby sa zvýšila jeho vizuálna dôležitosť a vytvorila sa jasná hierarchia prvkov. Pomáhajú zlepšiť čitateľnosť textu tým, že oddelia riadky alebo odseky a zvýraznia tak jednotlivé časti textu.
         </p>
         <div className="flex gap-4 py-2.5 pr-20 mt-10 text-custom-19 font-bold tracking-normal leading-7 whitespace-nowrap border-b border-neutral-200 max-md:flex-wrap max-md:pr-5">
