793488e7424e0086c30c9b4fac82fcc8caf15269
marosbar <maros.baranek@csirt.sk>
2024-08-14 14:25:35 +0200

    added styling

--- src/app/(home)/_components/focus/radio.js
+++ src/app/(home)/_components/focus/radio.js
@@ -5,7 +5,7 @@ import React from 'react';
 const RadioExample = () => {
   return (
     <div className="flex gap-8 items-start self-start mt-8">
-      <div className="flex flex-col gap-2.5 justify-center p-0.5 text-custom-19 tracking-wide leading-7 text-black rounded-md border-amber-600 border-solid border-[3px] w-[198px]">
+      <div className="flex flex-col gap-2.5 justify-center p-0.5 text-custom-19 tracking-wide leading-7 text-black rounded-md border-amber-600 border-solid border-[3px] w-[200px]">
         <label className="flex gap-4 items-center pr-2.5">
           <input
             className="flex shrink-0 self-stretch my-auto w-10 h-10 bg-white border-2 border-black border-solid rounded-[1000px]"
