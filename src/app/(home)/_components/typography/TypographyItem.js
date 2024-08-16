3e0871fbbc3f1d3aa8fa19be200c7167ea2e80f6
bkatlovsky <branislav.katlovsky@mirri.gov.sk>
2024-08-16 16:21:42 +0200

    typography page minor fix

--- src/app/(home)/_components/typography/TypographyItem.js
+++ src/app/(home)/_components/typography/TypographyItem.js
@@ -8,7 +8,7 @@ const TypographyItem = ({ title, size, lineHeight, spacing, type, fontWeight }) 
     <div className="py-5 border-t border-b border-neutral-200 max-md:max-w-full">
       <div className="flex gap-5 max-md:flex-col">
         <div className="flex  w-[50%] max-md:ml-0 max-md:w-full">
-        <h3 
+          <h4 
             className={`text-${type.toLowerCase()} tracking-wide`} 
             style={{
               fontSize: size,
@@ -18,7 +18,7 @@ const TypographyItem = ({ title, size, lineHeight, spacing, type, fontWeight }) 
             }}
           >
             {title}
-          </h3>
+          </h4>
         </div>
         <div className="flex flex-col ml-35 w-[50%] max-md:ml-0 max-md:w-full">
           <p className="text-xs font-bold leading-4 text-neutral-500 max-md:mt-10">
