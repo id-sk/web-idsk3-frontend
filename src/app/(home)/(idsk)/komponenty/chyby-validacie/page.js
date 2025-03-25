b165f57f9270218e674cac83c1bae13860be304e
bkatlovsky <166816424+bkatlovsky@users.noreply.github.com>
2025-03-25 13:07:47 +0100

    Update page.js

--- src/app/(home)/(idsk)/komponenty/chyby-validacie/page.js
+++ src/app/(home)/(idsk)/komponenty/chyby-validacie/page.js
@@ -13,6 +13,11 @@ const ErrorValidationPage = () => {
     <p className="text-2xl leading-9 mb-16">
         V tejto časti nájdete popísané pravidlá ako zobrazovať chyby a validácie v elektronickom formulári. V prípade, že používateľ zle vyplní formulár, zobrazí sa chyba a formulár nie je možné odoslať. Základné pravidlo pri zobrazovaní chýb a validácii je viditeľné zobraziť a vysvetliť chyby, ktoré nastali.
     </p>
+    <div className="ml-2 mb-16">
+        <Link href={'https://komponenty.idsk3.gov.sk/components/error-summary'}>
+            <Card title="Kód komponentu" content="HTML verzia na prevzatie"/>
+        </Link>
+    </div>
     <h2 className="mb-4">
         Použitie chyby a validácie
     </h2>
