c1ed07f044c6147495baec083e6d8e412f08a039
ID-SK team <idsk@mirri.gov.sk>
2025-04-09 12:12:43 +0200

    Update FooterResp.js

--- src/app/(home)/_components/footer/FooterResp.js
+++ src/app/(home)/_components/footer/FooterResp.js
@@ -10,13 +10,13 @@ const FooterResp = () => {
       <div className="justify-center w-full max-w-[1120px] max-md:max-w-full">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
           <div className="flex flex-col w-[73%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col grow text-base tracking-wide leading-7 text-black max-md:mt-10 max-md:max-w-full">
-              <p className="leading-6 max-md:max-w-full">Na tomto webovom sídle sa využívajú len nevyhnutné/technické cookies.</p>
+            <div className="flex flex-col grow text-base tracking-wide text-black max-md:mt-10 max-md:max-w-full">
+              <p className="max-md:max-w-full">Na tomto webovom sídle sa využívajú len nevyhnutné/technické cookies.</p>
               <div className="mt-4 flex gap-5 max-md:flex-wrap">
                 <a href="https://idsk.gov.sk/co-je/cookies-policy" className="underline">Informácie o spracúvaní súborov cookies</a>
                 <a href="mailto:idsk@mirri.gov.sk" className="underline">Kontakt na prevádzkovateľa</a>
               </div>
-              <p className="mt-4 leading-6 max-md:max-w-full">Prevádzkovateľom služby je Ministerstvo investícií, regionálneho rozvoja a informatizácie SR.</p>
+              <p className="mt-4 max-md:max-w-full">Prevádzkovateľom služby je Ministerstvo investícií, regionálneho rozvoja a informatizácie SR.</p>
               <a href="https://idsk.gov.sk" className="mt-4 text-xl max-md:max-w-full">
                 <span className="text-base tracking-wide text-black">Vytvorené v súlade s </span>
                 <span className="text-base tracking-wide text-black underline"> IDSK.</span>
