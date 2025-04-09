ee743b6a12cd0eacf2be64d58eb4b1b24b0a084d
ID-SK team <idsk@mirri.gov.sk>
2025-04-09 12:11:28 +0200

    Update FooterResp.js

--- src/app/(home)/_components/footer/FooterResp.js
+++ src/app/(home)/_components/footer/FooterResp.js
@@ -11,15 +11,15 @@ const FooterResp = () => {
         <div className="flex gap-5 max-md:flex-col max-md:gap-0">
           <div className="flex flex-col w-[73%] max-md:ml-0 max-md:w-full">
             <div className="flex flex-col grow text-base tracking-wide leading-7 text-black max-md:mt-10 max-md:max-w-full">
-              <p className="mt-5 leading-6 max-md:max-w-full">Na tomto webovom sídle sa využívajú len nevyhnutné/technické cookies.</p>
-              <div className="flex gap-5 max-md:flex-wrap">
+              <p className="leading-6 max-md:max-w-full">Na tomto webovom sídle sa využívajú len nevyhnutné/technické cookies.</p>
+              <div className="mt-4 flex gap-5 max-md:flex-wrap">
                 <a href="https://idsk.gov.sk/co-je/cookies-policy" className="underline">Informácie o spracúvaní súborov cookies</a>
                 <a href="mailto:idsk@mirri.gov.sk" className="underline">Kontakt na prevádzkovateľa</a>
               </div>
-              <p className="mt-5 leading-6 max-md:max-w-full">Prevádzkovateľom služby je Ministerstvo investícií, regionálneho rozvoja a informatizácie SR.</p>
-              <a href="https://idsk.gov.sk" className="mt-5 text-xl max-md:max-w-full">
-                <span className="text-base tracking-wide leading-6 text-black">Vytvorené v súlade s </span>
-                <span className="text-base tracking-wide leading-7 text-black underline"> IDSK.</span>
+              <p className="mt-4 leading-6 max-md:max-w-full">Prevádzkovateľom služby je Ministerstvo investícií, regionálneho rozvoja a informatizácie SR.</p>
+              <a href="https://idsk.gov.sk" className="mt-4 text-xl max-md:max-w-full">
+                <span className="text-base tracking-wide text-black">Vytvorené v súlade s </span>
+                <span className="text-base tracking-wide text-black underline"> IDSK.</span>
               </a>
             </div>
           </div>
