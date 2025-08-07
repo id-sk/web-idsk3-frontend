34d75578cd5c2dae8dd29f045512559c5c55500f
bkatlovsky <166816424+bkatlovsky@users.noreply.github.com>
2025-08-07 07:58:22 +0000

    minor design fix

--- src/app/(home)/(idsk)/komponenty/razcestnik-textovy/page.js
+++ src/app/(home)/(idsk)/komponenty/razcestnik-textovy/page.js
@@ -18,7 +18,7 @@ const NavigationBarPage = () => {
         rozmiestnený na rôznych, samostatných podstránkach. Rázcestník je jedným z komponentov,
         ktoré navigujú používateľa k obsahu vašej stránky.
       </p>
-      <div className="mt-8">
+      <div className="ml-2 mb-16">
         <Link href={'https://komponenty.idsk3.gov.sk/components/signpost/'}>
           <ArticleCard>
             <h4>Kód komponentu</h4>
