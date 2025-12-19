c9219525ef7b0a7ebf7b33eaf53576d12a5535da
MarcelSedo <marcel.sedo1@gmail.com>
2025-12-19 09:08:20 +0000

    fix problemov

--- src/app/(home)/(whatIs)/co-je/navod-pre-dizajnerov/page.js
+++ src/app/(home)/(whatIs)/co-je/navod-pre-dizajnerov/page.js
@@ -1,49 +1,49 @@
-'use client'
+'use client';
 
+import Head from 'next/head';
 import { PrimaryButton } from '@eslovensko/idsk-react';
-import { useRouter } from "next/navigation";
-
 
 const DesignersPage = () => {
-    const router = useRouter();
-
-    const handleClick = () => {
-
-        router.push('https://www.figma.com/community/file/1581301778555425083');
-
-    };
-
-    return (
-        <div className="component-page">
-            <title>Pre dizajnérov</title>
-            <h1 className="my-8">
-                Pre dizajnérov
-            </h1>
-            <p className="idsk-subtitle mb-16">
-                V dizajnérskom nástroji Figma nájdete knižnicu dizajnového systému IDSK – knižnicu štýlov, komponentov či vzorov.
-
-                Všetky súbory sú voľne prístupné na prepoužitie a pre prácu s nimi je nevyhnutné duplikovať súbory alebo skopírovať do nového súboru.
-
-                Pomocou týchto komponentov je možné vyskladať elektronickú v službu a/alebo webové sídlo v IDSK.
-            </p>
-            <PrimaryButton
-                label="Knižnica IDSK"
-                onClick={handleClick}
-                className='my-4 ml-1'
-            />
-            <iframe
-                style="
-                    border: 1px solid rgba(0, 0, 0, 0.1);
-                    transform: scale(0.49);
-                    transform-origin: top left;
-                "
-                width="800"
-                height="450"
-                src="https://embed.figma.com/design/8zCidJGMqcU5Kdmd94BoEL/IDSK-3.1.0?node-id=5012-6464&embed-host=share"
-                allowfullscreen>
-            </iframe>
-        </div>
+  const handleClick = () => {
+    window.open(
+      'https://www.figma.com/community/file/1581301778555425083',
+      '_blank',
+      'noopener,noreferrer'
     );
-}
+  };
+
+  return (
+    <div className="component-page">
+      <Head>
+        <title>Pre dizajnérov</title>
+      </Head>
+
+      <h1 className="my-8">Pre dizajnérov</h1>
+
+      <p className="idsk-subtitle mb-16">
+        V dizajnérskom nástroji Figma nájdete knižnicu dizajnového systému IDSK –
+        knižnicu štýlov, komponentov či vzorov.
+      </p>
+
+      <PrimaryButton
+        label="Knižnica IDSK"
+        onClick={handleClick}
+        className="my-4 ml-1"
+      />
+
+      <iframe
+        style={{
+          border: '1px solid rgba(0,0,0,0.1)',
+          transform: 'scale(0.49)',
+          transformOrigin: 'top left',
+        }}
+        width={800}
+        height={450}
+        src="https://embed.figma.com/design/8zCidJGMqcU5Kdmd94BoEL/IDSK-3.1.0?node-id=5012-6464&embed-host=share"
+        allowFullScreen
+      />
+    </div>
+  );
+};
 
 export default DesignersPage;
