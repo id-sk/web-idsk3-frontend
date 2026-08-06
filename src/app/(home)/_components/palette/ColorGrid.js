1d4d4a6487f68eac0be3e09557c3a5b0025c243c
MarcelSedo <marcel.sedo1@gmail.com>
2026-08-06 14:05:50 +0000

    Fix po zmenách v next.js na základe auditu

--- src/app/(home)/_components/palette/ColorGrid.js
+++ src/app/(home)/_components/palette/ColorGrid.js
@@ -4,8 +4,11 @@ import ColorCard from './ColorCard';
 const ColorGrid = ({ colors }) => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6 mt-6 w-full">
-      {colors.map((color) => (
-        <ColorCard key="{color.hex}" {...color}/>
+      {colors.map((color, index) => (
+        <ColorCard
+          key={`${color.name}-${color.hex}-${index}`}
+          {...color}
+        />
       ))}
     </div>
   );
